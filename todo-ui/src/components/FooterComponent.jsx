767bbff603bfcf9e85ac3a182bbd01bb84aeb3ad
Almishev <antonalmishev@abv.bg>
2024-01-28 20:10:52 +0200

    commiting files

---
+++ todo-ui/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='text-center'>Anton Almishev 2024 Spring boot - React app</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
